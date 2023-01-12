a34f42e2c9d80f5437ac0f9bec8129100b2a1ea4
monnibhadu123 <106861189+monnibhadu123@users.noreply.github.com>
2023-01-12 18:08:26 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,47 @@
+import { AppBar, Container, Select, MenuItem, Toolbar, Typography} from "@mui/material"
+import {makeStyles} from "@mui/styles"
+import { useHistory } from "react-router-dom"
+import React from "react"
+
+const useStyles = makeStyles(()=>({
+  title: {
+    flex: 1,
+    color: "gold",
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+  },
+
+}));
+const Header = () => {
+  const classes = useStyles();
+
+  const history = useHistory();
+
+  return(
+    <AppBar color="transparent" position="static">
+      <Container>
+        <Toolbar>
+          <Typography onClick = {() => history.push("/")} 
+          className={classes.title}>
+            Crypyo Hunter
+            </Typography>
+
+          <Select variant="outlined" style={{
+            width: 100,
+            height: 40, 
+            marginLeft: 15,
+
+          }}
+          >
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"INR"}>INR</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    
+  )
+}
+export default Header
 

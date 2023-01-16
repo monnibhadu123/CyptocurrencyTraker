6a0106724b1eae68a93778108233e2d9ea13c31c
monnibhadu123 <106861189+monnibhadu123@users.noreply.github.com>
2023-01-16 15:25:51 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,72 @@
-import { AppBar, Container, Select, MenuItem, Toolbar, Typography} from "@mui/material"
-import {makeStyles} from "@mui/styles"
-import { useHistory } from "react-router-dom"
-import React from "react"
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+  ThemeProvider
+} from "@material-ui/core";
+import { createTheme, makeStyles } from "@material-ui/core/styles";
+import { useHistory } from "react-router-dom";
+import React from "react";
+import { CryptoState } from "../CryptoContext";
+// import { ThemeProvider } from "@mui/material";
 
-const useStyles = makeStyles(()=>({
+const useStyles = makeStyles(() => ({
   title: {
     flex: 1,
     color: "gold",
     fontFamily: "Montserrat",
     fontWeight: "bold",
-    cursor: "pointer",
-  },
-
+    cursor: "pointer"
+  }
 }));
+
+const darkTheme = createTheme({
+  palette: {
+    primar: {
+      main: "#fff"
+    },
+    type: "dark"
+  }
+});
+
 const Header = () => {
   const classes = useStyles();
-
+  const { currency, setCurrency } = CryptoState();
+  // console.log(currency);
   const history = useHistory();
 
-  return(
-    <AppBar color="transparent" position="static">
-      <Container>
-        <Toolbar>
-          <Typography onClick = {() => history.push("/")} 
-          className={classes.title}>
-            Crypyo Hunter
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            <Typography
+              onClick={() => history.push("/")}
+              className={classes.title}
+            >
+              Crypyo Hunter
             </Typography>
 
-          <Select variant="outlined" style={{
-            width: 100,
-            height: 40, 
-            marginLeft: 15,
-
-          }}
-          >
-            <MenuItem value={"USD"}>USD</MenuItem>
-            <MenuItem value={"INR"}>INR</MenuItem>
-          </Select>
-        </Toolbar>
-      </Container>
-    </AppBar>
-    
-  )
-}
-export default Header
-
+            <Select
+              variant="outlined"
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15
+              }}
+              value={currency}
+              onChange={(e) => setCurrency(e.target.value)}
+            >
+              <MenuItem value={"USD"}>USD</MenuItem>
+              <MenuItem value={"INR"}>INR</MenuItem>
+            </Select>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
+  );
+};
+export default Header;
